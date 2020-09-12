b0d61440f57017683b9bae168f1b298f6680f8d1
Karl Beck <karljohnbeck@gmail.com>
2020-09-11 22:55:53 -0500

    added style to the edit category page

--- src/components/EditCategory/EditCategory.js
+++ src/components/EditCategory/EditCategory.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
-import { withRouter,Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
 // material ui 
 import { Typography, InputLabel, MenuItem, Input, withStyles, Grid, Select, Button, Card, TextField } from '@material-ui/core';
@@ -13,37 +13,41 @@ import deleteIcon from './Icons/trash.png'
 
 
 const styles = {
-  gridContainer: {
-    paddingLeft: '10px',
-    paddingRight: '10px',
-  },
-  card: {
-    
-    minWidth: '400px'
-  },
-  cardAction: {
-    backgroundColor: 'yellow',
-  },
-  img: {
-    Width: '45px',
-    height: '45px',
-    
+    gridContainer: {
+        paddingLeft: '10px',
+        paddingRight: '10px',
+    },
+    card: {
+        minHeight: '65px',
+        minWidth: '400px'
+    },
+    cardAction: {
+        backgroundColor: 'yellow',
+    },
+    img: {
 
-    margin: '5px',
-    float: 'right'
+        marginTop: '10px',
 
-  },
-  text: {
-    float: 'left',
-    marginLeft: '10px',
+        Width: '45px',
+        height: '45px',
 
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  margin: {
-    margin: '5px'
-  },
+
+        margin: '5px',
+        float: 'right'
+
+    },
+    text: {
+        marginTop: '10px',
+        display: 'inline-block',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    pos: {
+        marginBottom: 12,
+    },
+    margin: {
+        margin: '5px'
+    },
 };
 
 // Basic class component structure for React with default state
@@ -51,83 +55,89 @@ const styles = {
 // the component name TemplateClass with the name for the new
 // component.
 class EditCategory extends Component {
-  state = {
-    heading: 'Categories Component',
-    isEdit: true,
-    name: this.props.category.name,
-  };
-
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  toggleState = () => {
-    this.setState({
-        isEdit: !this.state.isEdit
-    })
-}
-toggleStatePlus = () => {
-    this.setState({
-        isEdit: !this.state.isEdit,
+    state = {
+        heading: 'Categories Component',
+        isEdit: true,
         name: this.props.category.name,
-    })
-}
-deleteCategory = () => {
-    this.props.dispatch({type: 'DELETE_CATEGORY', payload: this.props.category.id})
-}
+    };
 
-saveEdit = () => {
-    console.log(this.state.name)
-    this.props.dispatch(
-        {type: 'EDIT_CATEGORY', 
-        payload: {
-            id: this.props.category.id,
-            name: this.state.name
-        }})
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    toggleState = () => {
+        this.setState({
+            isEdit: !this.state.isEdit
+        })
+    }
+    toggleStatePlus = () => {
+        this.setState({
+            isEdit: !this.state.isEdit,
+            name: this.props.category.name,
+        })
+    }
+    deleteCategory = () => {
+        this.props.dispatch({ type: 'DELETE_CATEGORY', payload: this.props.category.id })
+    }
+
+    saveEdit = () => {
+        console.log(this.state.name)
+        this.props.dispatch(
+            {
+                type: 'EDIT_CATEGORY',
+                payload: {
+                    id: this.props.category.id,
+                    name: this.state.name
+                }
+            })
         this.toggleState()
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+
+            <Card className={classes.card}>
+                {this.state.isEdit ?
+                    <div>
+                        <Typography
+                            alignItems="center"
+                            justify="center"
+                            variant="h4"
+                            className={classes.text}>
+                            {this.props.category.name}
+                        </Typography>
+                        <img className={classes.img} onClick={this.deleteCategory} src={deleteIcon} alt="delete" />
+                        <img className={classes.img} onClick={this.toggleState} alt="edit" src={editIcon} />
+
+                    </div>
+                    :
+                    <div>
+                        <TextField
+                            className={classes.margin}
+                            variant="filled"
+                            autoFocus
+                            name="name"
+                            value={this.state.name}
+                            margin="dense"
+                            id="name"
+                            label="Category name"
+                            type="text"
+                            justify="center"
+                            fullWidth
+                            onChange={this.handleChange}
+                        />
+                        <Button className={classes.margin} onClick={this.toggleStatePlus} variant="outlined">Cancel</Button>
+                        <Button className={classes.margin} onClick={this.saveEdit} variant="outlined">Save</Button>
+
+                    </div>
+                }
+            </Card>
+        );
+    }
 }
 
-  render() {
-    const { classes } = this.props;
-    return (
-
-      <Card className={classes.card}>
-          {this.state.isEdit ? 
-          <div>
-              <Typography justify="center" variant="h4" className={classes.text}>
-              {this.props.category.name}  
-              </Typography>           
-               <img className={classes.img} onClick={this.deleteCategory} src={deleteIcon} alt="delete"/>
-              <img className={classes.img} onClick={this.toggleState} alt="edit" src={editIcon}/>
-
-          </div> 
-          : 
-          <div>
-              <TextField
-              className={classes.margin}
-                        variant="filled"
-                        autoFocus
-                        name="name"
-                        value={this.state.name}
-                        margin="dense"
-                        id="name"
-                        label="Category name"
-                        type="text"
-                        justify="center"
-                        fullWidth
-                        onChange={this.handleChange}
-                    />
-                <Button className={classes.margin} onClick={this.toggleStatePlus} variant="outlined">Cancel</Button>
-                <Button className={classes.margin} onClick={this.saveEdit} variant="outlined">Save</Button>
-
-          </div>
-  }
-      </Card>
-    );
-  }
-}
-
-const withRouterEditCategory = withRouter(EditCategory) 
+const withRouterEditCategory = withRouter(EditCategory)
 
 const withStyleEditCategory = withStyles(styles)(withRouterEditCategory)
 
